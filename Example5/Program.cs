f5bc8a96e2901185c227f4d4be4a5d571ff42212
IvaFil <filipovichiv@yandex.ru>
2022-08-25 18:10:46 +0300

    IfElse

---
+++ Example5/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введи имя");
+string name = System.Console.ReadLine();
+
+if(name.ToLower() == "маша")
+{
+    System.Console.WriteLine("УРААААААААААААА");
+}
+else
+{
+    System.Console.WriteLine("Привет, "+ name);
+}
